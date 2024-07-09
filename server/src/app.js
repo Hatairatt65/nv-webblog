8a7df64203944e3168cf5865fc1867b57f97e1f3
WHmild04 <gmpwh04@gmail.com>
2024-07-09 10:08:43 +0700

    folder perpared

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,13 @@
-console.log('Hello nodejs server')
+//console.log('Hello nodejs server')
+let express = require('express')
+const app = express()
+ 
+app.get('/status', function (req, res){
+    res.send('Hatairat titnam 65315369 Kaimook')
+})
+ 
+let port = 8080
+ 
+app.listen(port, function (){
+    console.log('server running on ' + port)
+})
